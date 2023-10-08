24ad6ccb055bfadd78f6eb4c0a4249ea0ced4bf0
obake-fe <daichismash@gmail.com>
2023-10-08 19:25:35 +0900

    core: install @react-three/fiber

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  transpilePackages: ['three'],
 }
+
+const withTM = require('next-transpile-modules')(['three'])
+module.exports = withTM()
 
 module.exports = nextConfig
--- src/pages/learning/FirstThree.tsx
+++ src/pages/learning/FirstThree.tsx
@@ -1,11 +1,19 @@
-const FirstThree = () => {
+import { Canvas } from '@react-three/fiber'
 
-
+const  FirstThree = () => {
   return (
-    <div>
-      hoge
+    <div className="App">
+      <h2>First Render</h2>
+      <Canvas>
+        <mesh>
+          <boxGeometry args={[10, 10, 1]} />
+          <meshPhongMaterial />
+        </mesh>
+        <ambientLight args={[0xff0000]} intensity={0.1} />
+        <directionalLight position={[0, 0, 5]} intensity={0.5} />
+      </Canvas>
     </div>
   )
 }
 
-export default  FirstThree
+export default FirstThree
