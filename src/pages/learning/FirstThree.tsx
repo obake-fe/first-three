1ce162d8f68c1f65c19072dea70300830f624291
obake-fe <daichismash@gmail.com>
2023-10-09 00:34:41 +0900

    feat: planeObjectを作成

--- src/pages/learning/FirstThree.tsx
+++ src/pages/learning/FirstThree.tsx
@@ -3,7 +3,6 @@ import { Canvas } from '@react-three/fiber'
 const FirstThree = () => {
   return (
     <div className="App">
-      <h2>First Render</h2>
       <Canvas
         camera={{ fov: 45, near: 0.1, far: 1000, position: [-30, 40, 30] }}
         onCreated={(state) => state.gl.setClearColor(0xeeeeee)}
@@ -12,8 +11,14 @@ const FirstThree = () => {
           <sphereGeometry args={[4, 20, 20]} />
           <meshBasicMaterial args={[{ color: 0x7777ff, wireframe: true }]} />
         </mesh>
+        <mesh rotation={[-0.5 * Math.PI, 0, 0, 'XYZ']} position={[15, 0, 0]}>
+          <planeGeometry args={[60, 20]} />
+          <meshBasicMaterial args={[{ color: 0xcccccc }]} />
+        </mesh>
         <ambientLight args={[0xff0000]} intensity={0.1} />
         <directionalLight position={[0, 0, 5]} intensity={0.5} />
+        <axesHelper args={[20]} />
+        <gridHelper args={[20, 20, 0xff0000, 'teal']} />
       </Canvas>
     </div>
   )
