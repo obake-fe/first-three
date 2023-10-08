8429476c44ae57475f316c25fce4495207c1b734
obake-fe <daichismash@gmail.com>
2023-10-09 00:01:21 +0900

    feat: cameraを調整し、sphereObjectを作成

--- src/pages/learning/FirstThree.tsx
+++ src/pages/learning/FirstThree.tsx
@@ -4,10 +4,13 @@ const FirstThree = () => {
   return (
     <div className="App">
       <h2>First Render</h2>
-      <Canvas>
+      <Canvas
+        camera={{ fov: 45, near: 0.1, far: 1000, position: [-30, 40, 30] }}
+        onCreated={(state) => state.gl.setClearColor(0xeeeeee)}
+      >
         <mesh>
-          <boxGeometry args={[10, 10, 1]} />
-          <meshPhongMaterial />
+          <sphereGeometry args={[4, 20, 20]} />
+          <meshBasicMaterial args={[{ color: 0x7777ff, wireframe: true }]} />
         </mesh>
         <ambientLight args={[0xff0000]} intensity={0.1} />
         <directionalLight position={[0, 0, 5]} intensity={0.5} />
